41ac3c3a7a64671030a9ad012a698347a46fe608
dwitka <witkadavid@gmail.com>
2024-03-17 21:41:43 -0400

    Write two functions: get_coordinates(), get_points().

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import requests
 import itertools
+import math
+from sympy import solve, Symbol, simplify
+from sympy.abc import x,y
+
 
 ADDRESS_LIST=[]
+distances = {}
 
 def read_addresses(a_file='ADDRESSES'):
     """(File-->List)
@@ -57,17 +62,16 @@ def distance_dictionary(struct_list):
     """(List(Lists)-->Dictionary)
     Structure the data in a dictionary for quick look-up of distance between cities.
     {'ROSE WATER ESTATES':  [{'ACCESS STORAGE': 5041, 'WALMART': 150331, 'DOWN RIVER POOLS': 141913}]}"""
-    points = {}
     for x in struct_list:
-        if not x[0] in points.keys():
-            points[x[0]] = [{x[1]: x[2]}]
+        if not x[0] in distances.keys():
+            distances[x[0]] = [{x[1]: x[2]}]
         else:
-            points[x[0]][0][x[1]] = x[2]     
-        if not x[1] in points.keys():
-            points[x[1]] = [{x[0]: x[2]}]
+            distances[x[0]][0][x[1]] = x[2]     
+        if not x[1] in distances.keys():
+            distances[x[1]] = [{x[0]: x[2]}]
         else:
-            points[x[1]][0][x[0]] = x[2]
-    return points
+            distances[x[1]][0][x[0]] = x[2]
+    return distances
 
 def get_coordinates(struct_list):
     """(List-->)
@@ -75,21 +79,42 @@ def get_coordinates(struct_list):
     calculate the coordinates for each city. First coordinates are (0,0) for first city and (0,y)
     second city of your choice. y = distance between first and second cities.
     struct_list=[[city1,city2,distance], [city1,..],..]"""
-    first_city = struct_list[0][0]
-    second_city = struct_list[0][1]
-    y = struct_list[0][2]
-    points = {first_city: (0,0), second_city: (0,y)}
-    for item in struct_list[2:]:
-        get_points(first_city, second_city, item)
-    return None
+    new_list = []
+    for address in ADDRESS_LIST:
+        new_list.append(address.split(',')[0])
+    location1 = new_list[0]
+    location2 = new_list[1]
+    y = distances[location1][0][location2]/1000
+    points_dictionary = {location1: (0,0), location2: (0,y)}
+    for location in new_list[2:]:
+        points = get_points(location1, location2, location)
+        points_dictionary[location] = points
+    return points_dictionary
+
+def get_points(city1, city2, city3):
+    d1 = distances[city3][0][city1]
+    d2 = distances[city2][0][city1]
+    d3 = distances[city3][0][city2]
+    x = Symbol("x", positive=True)
+    y = Symbol("y", positive=True)
+    eqs = (x**2 + y**2 - d1**2, x**2 + (y - d2)**2 - d3**2)
+    solutions = solve(eqs,x,y)
+    value1 = str(solutions[0][0])
+    value2 = str(solutions[0][1])
+    value1 = value1.replace("sqrt", "math.sqrt")
+    value2 = value2.replace("sqrt", "math.sqrt")
+    
+    solutions = ( eval(value1)/1000, eval(value2)/1000 )
+    print(solutions)
+    return solutions
+
 
 if __name__ == "__main__":
     data = structure_data(refine_data(get_response(create_request_string(read_addresses()))))
-    for every_list in data:
-        print(every_list)
-    dictionary = points_dictionary(data)
-    for x in dictionary:
-        print(x + ': ', dictionary[x])
+    distance_dictionary(data)
+    print(get_coordinates(data))
+    ADDRESS_LIST=[]
+    distances = {}
 
             
 
