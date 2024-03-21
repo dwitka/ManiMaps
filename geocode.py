ad1969b8d60e7affc10d629dee1ef0a6cf816b88
dwitka <witkadavid@gmail.com>
2024-03-20 21:38:45 -0400

    add 5 functions to module that retrieve longitude, latitude coordinates from Geocoding API.

---
+++ geocode.py
@@ -0,0 +1,40 @@
+import requests
+
+API_KEY = None
+
+def read_addresses(a_file='ADDRESSES'):
+    """(File-->List)
+    Read addresses from text file."""
+    file1 = open(a_file, 'r')
+    lines = file1.readlines()
+    file1.close()
+    return lines
+
+def create_request_string(address):
+    """(List-->String)
+    Create a cURL request string"""
+    url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+    address = 'address=' + address.replace(" ", "+")
+    api_key = '&key=' + API_KEY
+    return url + address + api_key
+
+def get_response(request_string):
+    response = requests.post(request_string)
+    return response.json()
+
+def make_geocode_list(lines):
+    geocode_list = []
+    for line in lines:
+        line = line.strip('\n')
+        request_string = create_request_string(line)
+        response = get_response(request_string)
+        coordinates = get_coordinates(response)
+        geocode_list.append(coordinates)
+    return geocode_list
+
+def get_coordinates(response):
+    geocodes = response['results'][0]['geometry']['location']
+    return geocodes
+
+if __name__ == "__main__":
+    print(make_geocode_list(read_addresses()))
