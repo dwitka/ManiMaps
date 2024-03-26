57fd75f1e434150f8c338384ebfa433012460abb
dwitka <witkadavid@gmail.com>
2024-03-25 22:07:08 -0400

    function run_module() and commenting

--- durations.py
+++ durations.py
@@ -1,13 +1,22 @@
+"""
+**durations** module extracts duration times between addresses.
+
+**durations** reads a standard text file where each line consists of an 
+address, it builds an http request and sends it to google's 
+servers.
+
+Output returns matrix of duration time in seconds.
+"""
+
 import requests
-import itertools
-import numpy as np
 
 API_KEY = None
 ADDRESS_LIST=[]
 
 def read_addresses(a_file='ADDRESSES'):
     """(File-->List)
-    Read addresses from text file."""
+    Read addresses from text file.
+    """
     file1 = open(a_file, 'r')
     lines = file1.readlines()
     file1.close()
@@ -15,7 +24,8 @@ def read_addresses(a_file='ADDRESSES'):
 
 def create_request_string(lines):
     """(List-->JSON)
-    Create json data for a cURL request"""
+    Create json data for a cURL request
+    """
     json_data = {'origins': [], 'destinations': [],'travelMode': 'DRIVE'}
     for line in lines:
         line = line.strip('\n')
@@ -27,7 +37,8 @@ def create_request_string(lines):
 
 def get_response(json_data):
     """(JSON-->Response)
-    Build cURL request and send."""
+    Build cURL request and send.
+    """
     headers = {
         'Content-Type': 'application/json',
         'X-Goog-Api-Key': API_KEY,
@@ -37,6 +48,9 @@ def get_response(json_data):
     return response.json()
 
 def durations_matrix(response):
+    """(Response-->Matrix)
+    Returns an asymmetric durations matrix.
+    """
     num = len(ADDRESS_LIST)
     durations_list = []
     for x in range(num):
@@ -45,11 +59,16 @@ def durations_matrix(response):
         durations_list[dictionary['originIndex']][dictionary['destinationIndex']] = int(dictionary['duration'].strip('s'))
     return durations_list
 
-if __name__ == "__main__":
+def run_module():
+    """(None-->Matrix)
+    Run the module from within another module or from the python shell.
+    """
     lines = read_addresses()
     json_data = create_request_string(lines)
     response = get_response(json_data)
     matrix = durations_matrix(response)
-    matrix = np.matrix(matrix)
-    print(matrix)
+    return matrix
+
+if __name__ == "__main__":
+    print(run_module())
 
