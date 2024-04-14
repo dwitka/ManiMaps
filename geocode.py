04d6d7f1be64f5e8f5e6ff928824f721bdce6d8f
dwitka <witkadavid@gmail.com>
2024-04-14 10:58:56 -0400

    embed subroutine function into run()

--- geocode.py
+++ geocode.py
@@ -6,7 +6,7 @@ from an address file.
 of an address, it builds an http request and sends it to google's 
 servers.
 
-make_geocode_list parses out longitude and latitude coordinates 
+get _coordinates parses out longitude and latitude coordinates 
 from the response and returns them in a list.
 
     [[longitude, latitude],[longitude, latitude],...]
@@ -34,37 +34,32 @@ def create_request_string(address: list) -> str:
 def get_response(request_string: str) -> "response":
     """ Get a json response from google's servers. """
     response = requests.post(request_string)
-    return response.json()
-
-
-def geocodes(line: str) -> list:
-    """ longitude and latitude set """
-    line.strip('\n')
-    request_string = create_request_string(line)
-    response = get_response(request_string)
-    coordinates = get_coordinates(response)
-    coordinates = get_list_coordinates(coordinates)
-    return coordinates
-
-
-def get_list_coordinates(coordinates: tuple) -> list:
-    """ Convert tuple to a list. """
-    return [coordinates['lat'], coordinates['lng']]   
+    return response.json()   
 
 
 def get_coordinates(response: "response") -> dict:
     """ Retrieve geocodes from json response. """
-    geocodes = response['results'][0]['geometry']['location']
-    return geocodes
+    coordinates = response['results'][0]['geometry']['location']
+    return [ coordinates['lat'], coordinates['lng'] ]
 
 
-def run_module() -> list:
+def run() -> list:
     """ Run the module from within another module or from the python shell. """
+    
+    def subroutine(line: str) -> list:
+        """ longitude and latitude set """
+        line.strip('\n')
+        request_string = create_request_string(line)
+        response = get_response(request_string)
+        coordinates = get_coordinates(response)
+        return coordinates
+
     lines = read_addresses()
-    return [ geocodes(line) for line in lines ]
+    return [ subroutine(line) for line in lines ]
+
 
 if __name__ == "__main__":
-    print(run_module())
+    print(run())
 
 
 
